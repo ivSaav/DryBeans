ac6bcfa2d92989b14755c24900d448c3ef690fdc
ivSaav <ivo_saavedra@outlook.com>
2021-04-30 12:02:43 +0100

    adding resources

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-import os
-
-
-beans_data = pd.read_csv('../resources/Dry_Bean_Dataset.xlsx')
+beans_data = pd.read_csv('../resources/dataset.xlsx')
 
 def main():
     print("Dry Beans\n", beans_data)
