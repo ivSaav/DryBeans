4ead1b538db6405189b2c98d9dd322799c96fabc
up201806534 <up201806534.fe.up.pt>
2021-04-30 12:34:57 +0100

    🚧 plot

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+import matplotlib.pyplot as plt
+import seaborn as sb
+
 import pandas as pd
 
-beans_data = pd.read_csv('../resources/dataset.xlsx')
+beans_data = pd.read_excel('../resources/dataset.xlsx', na_values=['NA'])
 
 def main():
-    print("Dry Beans\n", beans_data)
-    
+    #print("Dry Beans\n\n", beans_data)
+
+    #GIVES STATISTICS 
+    #print(beans_data.describe())
+    #print(beans_data.head())
+
+    sb.pairplot(beans_data.dropna(), hue='Class')
+    plt.show()
+
 if __name__ == "__main__":
     main()
