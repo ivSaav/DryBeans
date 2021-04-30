d8e22976ed947b092ccca66498fb91e36e6b89a7
ivSaav <ivo_saavedra@outlook.com>
2021-04-30 12:00:29 +0100

    opening csv

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import pandas as pd
 
+import os
+
+
+beans_data = pd.read_csv('../resources/Dry_Bean_Dataset.xlsx')
 
 def main():
-    print("Dry Beans")
+    print("Dry Beans\n", beans_data)
     
 if __name__ == "__main__":
     main()
