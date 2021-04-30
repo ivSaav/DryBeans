ad06e21f7646a9551ed2fddf712efb5d9434a539
ivSaav <ivo_saavedra@outlook.com>
2021-04-30 17:25:14 +0100

    Ploting seker beans data; Fix: fixed resources path

--- src/main.py
+++ src/main.py
@@ -3,17 +3,22 @@ import seaborn as sb
 
 import pandas as pd
 
-beans_data = pd.read_excel('../resources/dataset.xlsx', na_values=['NA'])
 
 def main():
     #print("Dry Beans\n\n", beans_data)
 
     #GIVES STATISTICS 
     #print(beans_data.describe())
-    #print(beans_data.head())
+    
+    beans_data = pd.read_excel('./resources/dataset.xlsx', na_values=['NA'])
+    
+    print(beans_data.head())
+    
+    # select SEKER class beans
+    seker_data = beans_data.loc[beans_data['Class'] == 'SEKER']
 
-    sb.pairplot(beans_data.dropna(), hue='Class')
-    plt.show()
+    sb.pairplot(seker_data.dropna(), hue='Class')
+    plt.savefig('out.png')
 
 if __name__ == "__main__":
     main()
