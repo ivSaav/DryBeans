045438b708517048c4a2cb6fbf07845580d60849
BrenoAccioly <breno.accioly91@gmail.com>
2021-05-06 20:21:48 +0100

    Added violin plot

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
+import math
 import matplotlib.pyplot as plt
 import seaborn as sb
-
 import pandas as pd
 
 
@@ -10,14 +10,31 @@ def plot_class_data(ds, class_name, out_file, out_dir='./output/'):
     sb.pairplot(data.dropna(), hue='Class')
     plt.savefig(f'{out_dir}{out_file}.png')
     
+def violin_plot(data, columns, out_file, out_dir='./output/'):
+    plt.figure(figsize=(14, 10))
+
+    if len(columns) == 0: 
+        columns = data.columns
+    
+    plot_cols = math.ceil(math.sqrt(len(columns)))
+    plot_rows = max(1, math.ceil(len(columns)/plot_cols))
+
+    for column_index, column in enumerate(columns):
+        if column=='Class':
+            continue
+        plt.subplot(plot_rows, plot_cols, column_index + 1)
+        sb.violinplot(x='Class', y=column, data=data)
+    plt.savefig(f'{out_dir}{out_file}.png')
+
 def main():
     #print("Dry Beans\n\n", beans_data)
 
     #GIVES STATISTICS 
     #print(beans_data.describe())
-        
-    beans_data = pd.read_excel('./resources/dataset.xlsx', na_values=['NA'])
-    
+
+    beans_data = pd.read_excel('./resources/dataset.xlsx', na_values=['NA'])  
+    #beans_data = pd.read_excel('./resources/dataset.xlsx', na_values=['NA'], engine='openpyxl')
+
     # null values verification
     n = beans_data.isnull().sum().sum()
     if n > 0:
@@ -28,6 +45,10 @@ def main():
     
     print("=== DESCRIBE ===")
     print(beans_data.describe())
+
+    violin_plot(beans_data, [], 'violin_all')
+    #violin_plot(beans_data, ['Area', 'Eccentricity', 'Roundness', 'Solidity'], 'violin')
+    violin_plot(beans_data, ['ShapeFactor1', 'ShapeFactor2', 'ShapeFactor3', 'ShapeFactor4'], 'violin_shapefactors')
     
     # plot_class_data(beans_data, 'SEKER', 'barbunya_orig')
     # plot_class_data(beans_data, 'BARBUNYA', 'barbunya_orig')
