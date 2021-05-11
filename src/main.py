3b7f95ea08c3acf8db26e386f311900dfef595cd
ivSaav <ivo_saavedra@outlook.com>
2021-05-11 18:14:04 +0100

    Fix: dividing data into test and training sets to be used in functions; Add: started neural network classification

--- src/main.py
+++ src/main.py
@@ -4,16 +4,14 @@ import seaborn as sb
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
-def dfs(beans):
-    X, Y = beans.iloc[:, :-1], beans.iloc[:, -1]
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, train_size=0.8)
-    
+def dfs(beans, X, y, X_train, X_test, y_train, y_test):
     clf = tree.DecisionTreeClassifier()
     
     clf = clf.fit(X_test, y_test)
     
-    plt.figure(figsize=(20, 20), dpi=500)
+    plt.figure(figsize=(30, 30), dpi=500)
     tree.plot_tree(clf, feature_names=beans.columns[:-1].tolist(),class_names=pd.unique(y_test)) 
    
     plt.savefig('tree.png') 
@@ -21,7 +19,14 @@ def dfs(beans):
     # print(beans.columns[:-1])
     # r = tree.export_text(clf, feature_names=beans.columns[:-1].tolist())
     # print(r)
-    print("accuracy: ", clf.score(X, Y))
+    print("accuracy: ", clf.score(X, y))
+    
+
+def neural(beans, X,y, X_train, X_test, y_train, y_test):
+    clf = MLPClassifier().fit(X_train, y_train)
+    clf.score(X_test, y_test)  
+    print("accuracy: ", clf.score(X, y))  
+    
     
 
 def main():
@@ -32,10 +37,15 @@ def main():
     n = beans.isnull().sum().sum()
     if n > 0:
         print(f'[!] {n} values are missing.')
+
+    # extract values and class labels
+    X, y = beans.iloc[:, :-1], beans.iloc[:, -1]
+    # split values into testing and training datasets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
     
-    dfs(beans)
-    
-    
+    # dfs(beans, X, y, X_train, X_test, y_train, y_test)
+     
+    neural(beans, X,y, X_train, X_test, y_train, y_test)
    
     
     
