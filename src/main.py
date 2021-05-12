6f2697595f2561931613676c9f319a15bbccb855
ivSaav <ivo_saavedra@outlook.com>
2021-05-12 15:37:07 +0100

    Add: Standard scalar for neural network classifier; Add: classification report; Add: plot_confusion_matrix; Fix: mlp classifier now using 'adam' solver

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import plot_confusion_matrix, classification_report
 
 def dfs(beans, X, y, X_train, X_test, y_train, y_test):
     clf = tree.DecisionTreeClassifier()
@@ -22,10 +25,29 @@ def dfs(beans, X, y, X_train, X_test, y_train, y_test):
     print("accuracy: ", clf.score(X, y))
     
 
-def neural(beans, X,y, X_train, X_test, y_train, y_test):
-    clf = MLPClassifier().fit(X_train, y_train)
-    clf.score(X_test, y_test)  
-    print("accuracy: ", clf.score(X, y))  
+def neural(beans, X, y, X_train, X_test, y_train, y_test):
+    
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    clf = MLPClassifier(solver='adam', max_iter=500)
+    clf.fit(X_train, y_train)
+    
+    # generate predictions
+    predictions = clf.predict(X_test)
+    print("\nClassification Report ========= ")
+    print(classification_report(y_test, predictions))
+    
+    # score model
+    print("Score: ", clf.score(X_test, y_test))  
+    
+    # show confusion matrix
+    plot_confusion_matrix(clf, X_test, y_test)
+    plt.show()
+     
     
     
 
@@ -41,11 +63,11 @@ def main():
     # extract values and class labels
     X, y = beans.iloc[:, :-1], beans.iloc[:, -1]
     # split values into testing and training datasets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=5)
     
     # dfs(beans, X, y, X_train, X_test, y_train, y_test)
      
-    neural(beans, X,y, X_train, X_test, y_train, y_test)
+    neural(beans, X, y, X_train, X_test, y_train, y_test)
    
     
     
